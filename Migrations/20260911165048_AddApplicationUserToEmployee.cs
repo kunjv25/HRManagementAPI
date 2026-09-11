5415820e2e88d163c7ec1f05c02ed090371654f2
kunjv25 <kunjv25@gmail.com>
2026-09-11 22:42:39 +0530

    migration added column in employee

---
+++ Migrations/20260911165048_AddApplicationUserToEmployee.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApplicationUserToEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "Employees",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_ApplicationUserId",
+                table: "Employees",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_AspNetUsers_ApplicationUserId",
+                table: "Employees",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_AspNetUsers_ApplicationUserId",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_ApplicationUserId",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "Employees");
+        }
+    }
+}
